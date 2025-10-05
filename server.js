e4b6f516c97d04799479d701ee2b9e65f3f09538
rashedul <rashedul360rs@gmail.com>
2025-10-05 10:27:38 +0600

    access: to netlify hosted url ~  2nd update

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_PROD],
+    origin: [process.env.FRONTEND_URL, process.envFRONTEND_URL_PROD],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
