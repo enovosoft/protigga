3d44396cadf3b5e784d1c26a831c5712c9c3c3f0
rashedul <rashedul360rs@gmail.com>
2025-10-05 12:24:06 +0600

    cookie updated ~ 3

--- utils/cookie_setter.js
+++ utils/cookie_setter.js
@@ -2,22 +2,14 @@
 const cookie_setter = (res, token, cookieName = 'token', options = {}) => {
   const isProduction = process.env.NODE_ENV === 'production';
 
-  // res.cookie(cookieName, token, {
-  //   httpOnly: true, // secure from JS
-  //   secure: isProduction, // HTTPS required for prod
-  //   sameSite: isProduction ? 'none' : 'lax', // allow cross-domain
-  //   maxAge: options.maxAge || 1000 * 60 * 60 * 24, // 1 day default
-  //   path: '/', // root path
-  //   ...options,
-  // });
-
-  const cookieConfigs = {
-    httpOnly: false,
-    sameSite: 'none',
-    secure: true,
-    maxAge: options?.maxAge || 365 * 24 * 60 * 60 * 1000, // one year
-  };
-  res.cookie(cookieName, token, cookieConfigs);
+  res.cookie(cookieName, token, {
+    httpOnly: true, // secure from JS
+    secure: isProduction, // HTTPS required for prod
+    sameSite: isProduction ? 'none' : 'lax', // allow cross-domain
+    maxAge: options.maxAge || 1000 * 60 * 60 * 24, // 1 day default
+    path: '/', // root path
+    ...options,
+  });
 };
 
 module.exports = cookie_setter;
