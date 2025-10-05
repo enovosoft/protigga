3130689aeb77db91c2f5c40bbe218b23f023855c
rashedul <rashedul360rs@gmail.com>
2025-10-05 14:20:56 +0600

    cookie_setter: updateed httponly property

--- utils/cookie_setter.js
+++ utils/cookie_setter.js
@@ -3,7 +3,7 @@ const cookie_setter = (res, token, cookieName = 'token', options = {}) => {
   const isProduction = process.env.NODE_ENV === 'production';
 
   res.cookie(cookieName, token, {
-    httpOnly: true, // secure from JS
+    httpOnly: isProduction, // secure from JS
     secure: isProduction, // HTTPS required for prod
     sameSite: isProduction ? 'none' : 'lax', // allow cross-domain
     maxAge: options.maxAge || 1000 * 60 * 60 * 24, // 1 day default
