b6fee54d63aa445a11aa39c420fd27d101fda05d
rashedul <rashedul360rs@gmail.com>
2025-10-24 16:00:21 +0600

    feature added: if user blocked by admin cookie will be clearned

--- middlewares/is_blocked.js
+++ middlewares/is_blocked.js
@@ -1,4 +1,5 @@
 const checkUserExists = require('../utils/checkUserExists');
+const clearCookie = require('../utils/clearCookie');
 const responseGenerator = require('../utils/responseGenerator');
 
 const is_blocked = async (req, res, next) => {
@@ -6,6 +7,8 @@ const is_blocked = async (req, res, next) => {
     const { phone } = req.decoded_user ? req.decoded_user : req.body || {};
     const { user } = await checkUserExists({ phone });
     if (user?.is_blocked) {
+      clearCookie(res, 'access_token');
+      clearCookie(res, 'refresh_token');
       return responseGenerator(400, res, {
         message: 'Your account blocked by authority. please contact with them.',
         error: true,
