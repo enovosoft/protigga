17aa57d4b7d59f50f7c404fb39865857a4a48d43
rashedul <rashedul360rs@gmail.com>
2025-11-07 12:36:35 +0600

    added new feature: prevpassword and new password must be different when changing password

--- controllers/change_password/change_password_controller.js
+++ controllers/change_password/change_password_controller.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const checkUserExists = require('../../utils/checkUserExists');
 const responseGenerator = require('../../utils/responseGenerator');
 const normalizePhoneNumber = require('../../utils/normalize_phone_number');
+const clearCookie = require('../../utils/clearCookie');
 const change_password_controller = async (req, res, next) => {
   try {
     const { phone, password, prev_password } = req.body || {};
@@ -20,6 +21,16 @@ const change_password_controller = async (req, res, next) => {
         success: false,
         error: true,
       });
+
+    // ============= check prevpass == newpass
+    const is_prev_and_new_matched = bcrypt.compareSync(password, user.password);
+    if (is_prev_and_new_matched)
+      return responseGenerator(401, res, {
+        message: 'previous and new password must be different',
+        success: false,
+        error: true,
+      });
+
     //================== update: password
     const updated_data = await prisma.user.update({
       where: { phone },
@@ -27,12 +38,15 @@ const change_password_controller = async (req, res, next) => {
         password: bcrypt.hashSync(password, 10),
       },
     });
-    if (updated_data?.phone)
+    if (updated_data?.phone) {
+      clearCookie(res, 'access_token');
+      clearCookie(res, 'refresh_token');
       return responseGenerator(201, res, {
         success: true,
         error: false,
         message: 'password updated successfully',
       });
+    }
   } catch (error) {
     return next(error);
   }
